1e22ddc449cdc0da58cea7f5106d313a7db5ad01
Pavlo Karpenko <karpenkopasha19@outlook.com>
2023-09-17 13:05:51 +0300

    сhanges to files 17.09.2023

--- js/script.js
+++ js/script.js
@@ -6,25 +6,25 @@ document.addEventListener('DOMContentLoaded', function() {
       languageList.style.display = (languageList.style.display === 'block') ? 'none' : 'block';
     });
   
-    // Обработка выбора языка
+    // Handling Language Selection
     var languageLinks = document.querySelectorAll('.language-list a');
     for (var i = 0; i < languageLinks.length; i++) {
       languageLinks[i].addEventListener('click', function(event) {
         event.preventDefault();
         var selectedLanguage = this.getAttribute('data-lang');
-        // Здесь можно добавить логику для смены языка
-        // Например, загрузить соответствующий файл перевода или изменить тексты на странице
-        // Закрыть меню выбора языка
+        // Close the language selection menu
         languageList.style.display = 'none';
-        // Обновить текущий язык
+        // Update the current language
         currentLanguage.querySelector('span').textContent = this.textContent;
       });
     }
   
-    // Закрыть меню выбора языка при клике вне него
+    // Close the language selection menu when clicking outside of it
     document.addEventListener('click', function(event) {
       if (!currentLanguage.contains(event.target)) {
         languageList.style.display = 'none';
       }
     });
   });
+
+  /**/
